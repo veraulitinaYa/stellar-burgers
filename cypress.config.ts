ef71670dcc4510c1ed9bb2cbc35269d98e36eb69
veraulitinaYa <veraulitina@yandex.ru>
2026-01-29 19:51:41 +0300

    cypress tests draft, some tests work

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:4000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
   },
 });
--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -14,7 +14,12 @@ export const ModalUI: FC<TModalUIProps> = memo(
           <h3 className={`${styles.title} text text_type_main-large`}>
             {title}
           </h3>
-          <button className={styles.button} type='button'>
+          <button
+            className={styles.button}
+            type='button'
+            onClick={onClose}
+            data-testid='modal-close'
+          >
             <CloseIcon type='primary' onClick={onClose} />
           </button>
         </div>
